d3852f6117b0a8c64a2ed5cdd0ebeb6da4048cb8
khalil <khalil.pan2@gmail.com>
2020-11-29 14:05:30 -0300

    adding user data to firestore

--- app/src/main/java/com/easydoso_profissional/Register.java
+++ app/src/main/java/com/easydoso_profissional/Register.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,9 +16,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
@@ -26,6 +33,9 @@ public class Register extends AppCompatActivity {
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+
+    //get instance of firestore
+    FirebaseFirestore fStore;
 
 
     @Override
@@ -44,8 +54,11 @@ public class Register extends AppCompatActivity {
         mPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
         progressBar = findViewById(R.id.progressBar);
-//to check if user is already logged in, if yes? no need to show register page and open directly Main activity
+
+        //to check if user is already logged in, if yes? no need to show register page and open directly Main activity
         if (fAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
@@ -56,6 +69,9 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String fullName = mFullName.getText().toString();
+                String phone = mPhone.getText().toString();
+                final String[] userID = new String[1];
 
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required");
@@ -73,12 +89,31 @@ public class Register extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
 
                 //Register user in FireBase
-
                 fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+
+                            userID[0] = fAuth.getCurrentUser().getUid();
+
+                            //store data in firestore
+
+                            //to create a collection if not exists-create a documents with current userID
+                            DocumentReference documentReference = fStore.collection("users").document(userID[0]);
+
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fullName", fullName);
+                            user.put("email", email);
+                            user.put("phone", phone);
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("SUCCESS","UserPrifile created for "+ userID);
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         } else {
                             Toast.makeText(Register.this, "Error." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
